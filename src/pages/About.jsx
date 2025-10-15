e59ec1e33eee523711da21dd3df1a4c49fa1dc48
Vishal Lovanshi <vishallovanshi6@gmail.com>
2025-10-16 00:16:24 +0530

    test the changes from user1 branch

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
   <div className="sm:w-1/2 p-5 ">
     <div className="text">
       <span className="text-gray-500 border-b-2 border-indigo-600 uppercase">
-        About us
+        About 
       </span>
       <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">
         About <span className="text-indigo-600">Our Company</span>
